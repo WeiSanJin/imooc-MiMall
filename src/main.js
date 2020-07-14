f5820fd57adeae2ed90e428b000d28306b4ae576
Wwx0410 <1162335221@qq.com>
2020-07-14 23:08:33 +0800

    项目环境设置

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
-import Vue from 'vue';
+import Vue from "vue";
 import router from "./router";
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import App from './App.vue';
-import env from './env.js';
+import axios from "axios";
+import VueAxios from "vue-axios";
+import App from "./App.vue";
+import env from "./env.js";
 
 // 根据前端的跨域方式做调整
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = "/api";
 axios.defaults.timeout = 8000;
 // 根据环境变量获取不同的请求地址
 axios.defaults.baseURL = env.baseURL;
 // 接口错误拦截
+// eslint-disable-next-line
 axios.interceptors.response.use((response) => {
   let res = response.data;
   if (res.status == 0) {
     return res.data;
   } else if (res.status == 10) {
-    window.location.href = '/#/login';
+    window.location.href = "/#/login";
   } else {
     alert(res.msg);
   }
