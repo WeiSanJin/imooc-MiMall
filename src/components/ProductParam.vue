ea701b26cf1377ac9d8507eed26eb9b42c8f19b0
Wwx0410 <1162335221@qq.com>
2020-07-22 15:48:02 +0800

    组件吸顶实现

---
+++ src/components/ProductParam.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="nav-bar" :class="{ is_fixed: isFixed }">
+    <div class="container">
+      <div class="pro-title">小米8</div>
+      <div class="pro-param">
+        <a href="javascript:;">概述</a>
+        <span>|</span>
+        <a href="javascript:;">参数</a>
+        <span>|</span>
+        <a href="javascript:;">用户评价</a>
+        <slot name="buy"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-bar",
+  data() {
+    return {
+      isFixed: false
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.initHeight);
+  },
+  methods: {
+    initHeight() {
+      let scrollTop =
+        window.pageXOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      this.isFixed = scrollTop > 152 ? true : false;
+    }
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.initHeight, false);
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
+
+.nav-bar {
+  height: 70px;
+  line-height: 70px;
+  border-top: 1px solid #e5e5e5;
+  background-color: $colorG;
+  &.is_fixed {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    box-shadow: 0 5px 5px $colorE;
+  }
+  .container {
+    @include flex();
+  }
+  .pro-title {
+    font-size: 18px;
+    color: $colorB;
+    font-weight: bold;
+  }
+  .pro-param {
+    font-size: $fontJ;
+    span {
+      margin: 0px 10px;
+    }
+    a {
+      color: $colorC;
+    }
+  }
+}
+</style>
--- src/views/product.vue
+++ src/views/product.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </product-param>
+    <div class="h">0123123</div>
+    <div class="h">0123123</div>
+    <div class="h">0123123</div>
+    <div class="h">0123123</div>
+    <div class="h">0123123</div>
+    <div class="h">0123123</div>
+    <div class="h">0123123</div>
   </div>
 </template>
 
 <script>
+import ProductParam from "./../components/ProductParam";
 export default {
-  name: "product"
+  name: "product",
+  components: {
+    ProductParam
+  }
 };
 </script>
+<style lang="scss">
+.product {
+  .h {
+    height: 200px;
+  }
+  button {
+    margin-left: 10px;
+  }
+}
+</style>
